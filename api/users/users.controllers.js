28594133f1d11e2fd421b1c1f2ffef4eb900d136
AbdulazizMurad <abdulaziz.murad2001@gmail.com>
2024-09-29 20:31:46 +0300

    authentication done

--- api/users/users.controllers.js
+++ api/users/users.controllers.js
@@ -1,16 +1,40 @@
 const User = require("../../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const saltRounds = 10;
+
+const generateToken = (user) => {
+  const payload = {
+    id: user._id,
+    username: user.username,
+  };
+  const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "1h" });
+  return token;
+};
+
+const hashPassword = async (password) => {
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
+  return hashedPassword;
+};
 
 exports.signup = async (req, res) => {
   try {
+    const { password } = req.body;
+    const securePassword = await hashPassword(password);
+    req.body.password = securePassword;
     const newUser = await User.create(req.body);
-    res.status(201).json(newUser);
+    const token = generateToken(newUser);
+    res.status(201).json({ token: token });
   } catch (err) {
+    console.log(err);
     next(err);
   }
 };
 
 exports.signin = async (req, res) => {
   try {
+    const token = generateToken(req.user);
+    res.status(201).json({ token: token });
   } catch (err) {
     res.status(500).json("Server Error");
   }
